5d3f902ea08e6bb87474655f7f64134d83e70e48
Koziel Klaudia <klaudia.koziel@gmail.com>
2018-08-28 11:38:12 +0200

    Promise structure implemented

--- src/server.js
+++ src/server.js
@@ -5,17 +5,21 @@ const INCOME = 'inc';
 const EXPENSE = 'exp';
 
 app.get('/incomes', (req, res) => {
-    PositionModel.find({}, (err, data) => {
-        const incomes = data.filter(position => position.positionSign === INCOME);
-        res.json(incomes);
-    })
+    PositionModel.find({})
+        .then(data => {
+            const incomes = data.filter(position => position.positionSign === INCOME);
+            res.json(incomes);
+        })
+        .catch(err => console.log(err));
 });
 
 app.get('/expenses', (req, res) => {
-    PositionModel.find({}, (err, data) => {
-        const expenses = data.filter(position => position.positionSign === EXPENSE);
-        res.json(expenses);
-    })
+    PositionModel.find({})
+        .then(data => {
+            const expenses = data.filter(position => position.positionSign === EXPENSE);
+            res.json(expenses);
+        })
+        .catch(err => console.log(err));
 });
 
 app.post('/', (req, res) => {
@@ -29,19 +33,16 @@ app.post('/', (req, res) => {
 });
 
 app.delete('/position/:id', (req, res) => {
-    PositionModel.deleteOne({ _id: req.params.id}, () => {
-        res.json({});
-    })
+    PositionModel.deleteOne({ _id: req.params.id})
+        .then(() => res.json({}))
 });
 
 app.delete('/income', (req, res) => {
-    PositionModel.deleteMany({ positionSign: INCOME}, () => {
-        res.json({});
-    });
+    PositionModel.deleteMany({ positionSign: INCOME})
+        .then(() => res.json({}))
 });
 
 app.delete('/expenses', (req, res) => {
-    PositionModel.deleteMany({ positionSign: EXPENSE}, () => {
-        res.json({});
-    });
+    PositionModel.deleteMany({ positionSign: EXPENSE})
+        .then(() => res.json({}))
 });
